4bd92a7a7c1edc0626d07ca390f63d07087965b6
David Vielhuber <david@vielhuber.de>
2024-01-01 13:00:25 +0100

    Improve subfolder handling.

--- _public/_pwa/manifest.json.php
+++ _public/_pwa/manifest.json.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
-$json = file_get_contents('_manifest.json');
-if (strpos(@$_SERVER['HTTP_HOST'], 'local') !== false) {
+$json = file_get_contents('manifest.json');
+if (strpos(@$_SERVER['REQUEST_URI'], 'hosea') === false) {
     $json = str_replace('%SUBPATH%', '', $json);
 } else {
     $json = str_replace('%SUBPATH%', '/hosea', $json);
