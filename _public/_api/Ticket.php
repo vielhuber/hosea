b2b7d1c414a2e1df1af147ba1860e2e745784e09
David Vielhuber <david@vielhuber.de>
2025-01-05 22:11:11 +0100

    - Rename 'FEIER_OFF_TAG' to 'FEIERTAG' in multiple SQL query conditions. - Add parentheses around conditions using both 'STR_TO_DATE' and 'INSTR' functions. - Ensure consistent SQL formatting by aligning condition structures with parentheses.

--- _public/_api/Ticket.php
+++ _public/_api/Ticket.php
@@ -92,17 +92,17 @@ class Ticket extends Api
                 ' .
             ($interval_prev === null
                 ? '1=1'
-                : 'INSTR(project,\'FEIER_OFF_TAG\') OR STR_TO_DATE(date,\'%d.%m.%y\') > DATE_SUB(NOW(), INTERVAL ' .
+                : '(INSTR(project,\'FEIERTAG\') OR STR_TO_DATE(date,\'%d.%m.%y\') > DATE_SUB(NOW(), INTERVAL ' .
                     $interval_prev .
-                    ' DAY)') .
+                    ' DAY))') .
             '
                 AND
                 ' .
             ($interval_next === null
                 ? '1=1'
-                : 'INSTR(project,\'FEIER_OFF_TAG\') OR STR_TO_DATE(date,\'%d.%m.%y\') < DATE_ADD(NOW(), INTERVAL ' .
+                : '(INSTR(project,\'FEIERTAG\') OR STR_TO_DATE(date,\'%d.%m.%y\') < DATE_ADD(NOW(), INTERVAL ' .
                     $interval_next .
-                    ' DAY)') .
+                    ' DAY))') .
             '
                 )
                 )
